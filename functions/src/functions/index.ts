f2a93247b3b9f480b33ac5871643c6e058b36af9
Lars Rudbeck Bak <larsbak@makeable.dk>
2020-07-18 22:46:04 +0200

    dynamically load functions

--- functions/src/functions/index.ts
+++ functions/src/functions/index.ts
@@ -1,6 +1,10 @@
 // is where the firebase functions live.
 // I have found that structuring the files into folders based on what invokes them works best.
-// Eg. firestore/users/onCreate.f.ts.
+// Eg. firestore/user/onCreate.f.ts.
 // I then prefer to have a plain nodejs file that dynamically imports these .f.ts files.
 // The typescript compiler can handle both nodejs and typescript files with a single configuration line.
 // Functions files should contain next to no code and tests could be omitted here.
+
+import load from "../load";
+
+export default load(__dirname);
