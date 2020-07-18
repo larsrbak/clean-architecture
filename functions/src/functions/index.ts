3d82290913b48c3624444105aad1bdc00b0a61e8
Lars Rudbeck Bak <larsbak@makeable.dk>
2020-07-18 23:38:13 +0200

    fix functions exports

--- functions/src/functions/index.ts
+++ functions/src/functions/index.ts
@@ -1,10 +1,10 @@
-// is where the firebase functions live.
+// is where the https functions live.
 // I have found that structuring the files into folders based on what invokes them works best.
-// Eg. firestore/user/onCreate.f.ts.
+// Eg. https/user/create.f.ts.
 // I then prefer to have a plain nodejs file that dynamically imports these .f.ts files.
 // The typescript compiler can handle both nodejs and typescript files with a single configuration line.
 // Functions files should contain next to no code and tests could be omitted here.
 
 import load from "../load";
 
-export default load(__dirname);
+module.exports = load(__dirname);
