b811e4dcca909cccfeb28d8e0b577a1de5b3edd2
Lars Rudbeck Bak <larsbak@makeable.dk>
2020-07-18 21:02:04 +0200

    sample user model

--- functions/src/user.ts
+++ functions/src/user.ts
@@ -2,3 +2,11 @@
 // Models should only perform simple changes.
 // For more complex actions use other classes that use that model.
 // Models can be placed at root level. Uncle bob: you should be able to identify the type of application from root level files.
+
+export class User {
+  public name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
