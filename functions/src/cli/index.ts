ba0955412b7d7fe72bfb53e862470435b1adc059
Lars Rudbeck Bak <larsbak@makeable.dk>
2020-07-18 22:42:13 +0200

    dynamically load and run commands

--- functions/src/cli/index.ts
+++ functions/src/cli/index.ts
@@ -1,3 +1,36 @@
 // Idea:
 // Load commands like functions.
 // Allow for listing of commands
+
+import load from "../load";
+
+interface Command {
+  (...args: string[]): Promise<unknown>;
+}
+
+function list(commands: { [key: string]: Command }) {
+  console.log("Available commands are:");
+  Object.keys(commands).forEach(function (command) {
+    console.log(`  - ${command}`);
+  });
+}
+
+async function run([name, ...rest]: string[]) {
+  const commands = load(__dirname, "c.js");
+
+  if (!name) {
+    console.log(`You must specify a command.`);
+    return list(commands);
+  }
+
+  const command = commands[name];
+
+  if (!command) {
+    console.log(`Command ${name} not found.`);
+    return list(commands);
+  }
+
+  await command(...rest);
+}
+
+run(process.argv.slice(2));
