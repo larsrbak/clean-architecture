02e6d235fac12bcb95b7cf57739abc49ea789c82
Lars Rudbeck Bak <larsbak@makeable.dk>
2020-07-18 22:45:35 +0200

    sample create user interactor

--- functions/src/interactors/CreateUser.ts
+++ functions/src/interactors/CreateUser.ts
@@ -1,16 +1,18 @@
 // provide some sample here
 
 import UserRepository from "../repositories/UserRepository";
-import { User } from "../user";
+import { User } from "../User";
 
 export class CreateUser {
-  private userRepository: UserRepository;
+  private readonly userRepository: UserRepository;
+  private readonly name: string;
 
-  constructor(userRepository: UserRepository) {
+  constructor(userRepository: UserRepository, name: string) {
     this.userRepository = userRepository;
+    this.name = name;
   }
 
-  async execute(): Promise<void> {
-    await this.userRepository.create("123", new User("Lars Bak"));
+  async execute(): Promise<User> {
+    return await this.userRepository.add(new User(this.name));
   }
 }
